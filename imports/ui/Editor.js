d54b2852c7bc5c06a42604702c3e04904b21a652
TheLazyDev <bhavarshs@gmail.com>
2017-06-27 16:55:09 +0530

    Style editor component

--- imports/ui/Editor.js
+++ imports/ui/Editor.js
@@ -61,11 +61,14 @@ export class Editor extends React.Component {
         
             return (
                 <div className="editor">
-                    <input value={this.state.title} placeholder="Your title here" onChange={this.handleTitleChange.bind(this)}/>
-                    <textarea value={this.state.body} placeholder='Your note here' onChange={this.handleBodyChange.bind(this)}>
+                    <input className="editor__title" value={this.state.title} placeholder="Your title here" onChange={this.handleTitleChange.bind(this)}/>
+                    <textarea className="editor__body" value={this.state.body} placeholder='Your note here' onChange={this.handleBodyChange.bind(this)}>
 
                     </textarea>
-                    <button onClick={this.handleRemoval.bind(this)}> Delete Note </button>
+                    <div>
+                        <button className="button button-secondary" onClick={this.handleRemoval.bind(this)}> Delete Note </button>
+                    </div>
+                    
                 </div>
             )
 
@@ -74,7 +77,7 @@ export class Editor extends React.Component {
 
             return (
                 <div className="editor">
-                    <p>{this.props.selectedNoteId ? 'Note not found' : 'Pick or create a note to get started'}</p>
+                    <p className='editor__message'>{this.props.selectedNoteId ? 'Note not found' : 'Pick or create a note to get started'}</p>
                 </div>
                 
             )
